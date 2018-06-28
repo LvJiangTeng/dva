29ab2bad5f08cde19bc664bbde8308937ffe0ce7
吕江腾 <lvjiangteng@126.com>
2018-06-28 16:47:34 +0800

    完成

--- app/src/index.js
+++ app/src/index.js
@@ -1,20 +1,71 @@
 import dva from 'dva';
 import { Router, Route, Switch } from 'dva/router';
-
+import {connect} from "react-redux"
+import { call, put, takeEvery, takeLatest } from 'redux-saga/effects'
+import key from 'keymaster';
+import styles from './index.less';
 // 1. Initialize
 const app = dva();
 
-// 2. Model
-// Remove the comment and define your model.
-//app.model({});
+app.model({
+  namespace: 'count',
+  subscriptions:{
+    keyboardWatcher({dispatch}){
+      key('⌘+up, ctrl+up',()=>{
+        dispatch({type:'count/add'})
+      })
+    }
+  },
+  effects:{
+      *add(action,{call,put}){
+        yield call (delay,1000);
+        yield put ({type:'minus'});
+      }
+  },
+  state: {
+    record : 0,
+    current: 0,
+  },
+  reducers:{
+    add(state){
+      const newCurrent=state.current+1;
+      return {
+        ...state,
+        record:newCurrent>state.record?newCurrent:state.record,
+        current:newCurrent
+      }
+    },
+    minus(state){
+      return{
+        ...state,current:state.current-1
+      }
+    }
+  }
+});
+ const delay=(timeout)=>{
+  return new Promise(resolve=>{
+    setTimeout(resolve,timeout)
+  })
+}
+const CountApp = ({count, dispatch}) => {
+  return (
+    <div className={styles.normal}>
+      <div className={styles.record}>Highest Record: {count.record}</div>
+      <div className={styles.current}>{count.current}</div>
+      <div className={styles.button}>
+        <button onClick={() => { dispatch({type: 'count/add'}); }}>+</button>
+      </div>
+    </div>
+  );
+};
+ const mapStateToProps = (state) => {
+    return {count:state.count}
+}
 
-// 3. Router
-const HomePage = () => <div>Hello Dva.</div>;
+const HomePage = connect(mapStateToProps)(CountApp);
 app.router(({ history }) =>
   <Router history={history}>
-    <Switch>
       <Route path="/" exact component={HomePage} />
-    </Switch>
   </Router>
 );
 
