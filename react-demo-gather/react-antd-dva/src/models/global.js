c670bac046e2f011d0f80739846c34a9e9f424cb
吕江腾 <lvjiangteng@126.com>
2018-08-30 22:10:29 +0800

    todo list

--- react-demo-gather/react-antd-dva/src/models/global.js
+++
@@ -1,7 +0,0 @@
-export default {
-  namespace: 'global',
-
-  state: {
-    notices: [],
-  },
-}
--- react-demo-gather/react-antd-dva/src/routes/ArticleList/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const ArticleList = (props) => (
-  <h2>List of Article</h2>
-);
-
-export default ArticleList;
