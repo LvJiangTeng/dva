b620e7cc83986d549c37eaa9b6828444385ed8a7
吕江腾 <lvjiangteng@126.com>
2018-08-30 17:51:05 +0800

    删除

--- dvaDemo/src/utils/request.js
+++ dvaDemo/src/utils/request.js
@@ -25,6 +25,10 @@ export default function request(url, options) {
   return fetch(url, options)
     .then(checkStatus)
     .then(parseJSON)
-    .then(data => ({ data }))
-    .catch(err => ({ err }));
+    .then(data => ({
+      data
+    }))
+    .catch(err => ({
+      err
+    }));
 }
