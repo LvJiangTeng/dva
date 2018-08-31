f3466d1ebe9967e77a8545b9f380a2f7c90f9b42
吕江腾 <lvjiangteng@126.com>
2018-08-31 14:24:40 +0800

    todolist

--- react-demo-gather/todolist/src/index.js
+++ react-demo-gather/todolist/src/index.js
@@ -1,19 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { createStore } from 'redux'
-import { Provider } from 'react-redux'
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import './index.css';
-import todoApp from './redux/reducers'
-let store = createStore(todoApp)
-let rootElement = document.getElementById('root')
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import "./index.css";
+import todoApp from "./redux/reducers";
+// let store = createStore(todoApp);
+// let rootElement = document.getElementById("root");
+// ReactDOM.render(
+//   <Provider store={store}>
+//     <App />
+//   </Provider>,
+//   rootElement
+// );
+let rootElement = document.getElementById("root");
+let store = createStore(todoApp);
 ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
-    rootElement
-)
-
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
+);
 // ReactDOM.render(<App />, document.getElementById('root'));
- registerServiceWorker();
+registerServiceWorker();
